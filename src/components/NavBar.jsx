291967466ffc3967e6bc0adc1b53be872741a5dd
selvasneka <selvasneka871@gmail.com>
2025-07-09 07:00:10 +0530

    Initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import "../css/navbar.css"
+
+function NavBar(){
+    return <nav className="navbar">
+       <div className="navbar-brand">
+        <Link to="/">CineScout - A React Movie Explorer App!</Link>
+       </div>
+       <div className="navbar-links">
+        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/favorites" className="nav-link">Favorites</Link>
+       </div>
+    </nav>
+}
+ 
+export default NavBar;
